34b783fe51ec2391ef762d5ee71d81effc988456
creativeloc <creativeloc@yahoo.com>
2026-04-17 02:19:53 -0400

    add images asset folder; started hero section; added h1 text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HeroSection from "@/components/home/hero/HeroSection"
+
 export default function Home() {
-  return <h1 className="h-[2000px]">Hello everybody</h1>
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
 }
