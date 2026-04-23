4e380e7f756b90028039dbe9fd3311d5f46013c0
creativeloc <creativeloc@yahoo.com>
2026-04-23 05:15:58 -0400

    rearrange component directories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/home/hero/HeroSection"
-import ResumeSection from "@/components/home/ResumeSection"
+import ResumeSection from "@/components/resume/ResumeSection"
 import ServiceSection from "@/components/home/services/ServiceSection"
 import ProjectSection from "@/components/projects/ProjectSection"
 import SkillsSection from "@/components/skills/SkillsSection"
