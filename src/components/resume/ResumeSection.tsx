de9f4eb007cc0cf96624f897467522aa088c8b22
creativeloc <creativeloc@yahoo.com>
2026-04-26 06:38:36 -0400

    added animation to resume section

--- src/components/resume/ResumeSection.tsx
+++ src/components/resume/ResumeSection.tsx
@@ -14,7 +14,7 @@ export default function ResumeSection() {
     <section id="resume" className="my-12">
       <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-10">
         {/* education section */}
-        <div>
+        <div data-aos="zoom-out">
           <Header title="My Education" as="h2" />
           <div className="space-y-6">
             <ResumeCard
@@ -37,8 +37,9 @@ export default function ResumeSection() {
             />
           </div>
         </div>
+
         {/* work experience section */}
-        <div>
+        <div data-aos="zoom-in" data-aos-delay="200">
           <Header title="My Work Experience" as="h2" />
           <div className="space-y-6">
             <ResumeCard
