9888d1e60a53fada37f4647cc1a70338268f1e18
Lawrence Caulker <locaulker@gmail.com>
2026-04-26 09:19:44 -0400

    added animations to the skills and testimonials sections

--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -46,6 +46,8 @@ export default function SkillsSection() {
           return (
             <div
               key={index}
+              data-aos="flip-right"
+              data-aos-delay={index * 200}
               className="bg-slate-900 text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition:hover:scale-110"
             >
               <div className="text-5xl text-gray-300">{skill.icon}</div>
--- src/components/testimonials/TestimonialsSection.tsx
+++ src/components/testimonials/TestimonialsSection.tsx
@@ -38,6 +38,8 @@ export default function TestimonialsSection() {
           {testimonials.map((testimonial, index) => {
             return (
               <div
+                data-aos="fade-right"
+                data-aos-delay={index * 200}
                 key={index}
                 className="bg-gray-800 border border-gray-700 rounded-xl p-6 shadow-lg"
               >
